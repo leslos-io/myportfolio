c1df432abf4030eed876fcf36b3bac388ab63093
Leslos <lesliesamafful@gmail.com>
2025-01-10 13:04:09 +0000

    initialize Next.js project with Tailwind CSS and basic layout

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+// app/page.tsx
+import Image from "next/image";
+import { ThemeSwitcher } from "./components/ThemeSwitcher";
+
+export default function Home() {
+  return (
+    <div>
+      hey
+    </div>
+  );
+}
