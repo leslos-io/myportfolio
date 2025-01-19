df7a6cd13136e0ae4dde40b572615a58abf900f3
leslos-io <lesliesamafful@gmail.com>
2025-01-19 10:40:10 +0000

    remove Navigation component from RootLayout for cleaner structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 // app/layout.tsx
 import type { Metadata } from "next";
 import "./globals.css";
-import Navigation from "@/components/Navigation";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className="dark bg-black">
-        <Navigation />
+        
         {children}
       </body>
     </html>
