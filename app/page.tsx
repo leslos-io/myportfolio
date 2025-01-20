a5d27d5f85f50bd9fa23b8b80a45d8f7e020f43d
Leslos <lesliesamafful@gmail.com>
2025-01-20 13:35:25 +0000

    add Spotlight component and SomeOfOurWork section with layout grid for enhanced visual presentation

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,21 @@
 import Hero from "@/components/Hero";
+import SomeOfOurWork from "@/components/SomeOfOurWork";
 import React from "react";
 
 const page = () => {
   return (
     <div className="h-screen snap-y snap-mandatory overflow-y-scroll">
-      
       <Hero />
 
       {/* Third Section - Green */}
-      <section className="h-screen snap-start bg-green-500 flex items-center justify-center">
+      <section className="h-screen snap-start bg-gradient-to-t from-zinc-950 via-zinc-800 to-zinc-900 flex items-center justify-center">
+        <SomeOfOurWork />
+      </section>
+
+      {/* Fourth Section - Blue */}
+      <section className="h-screen snap-start bg-blue-500 flex items-center justify-center">
         <div className="text-white text-4xl font-bold">
-          Your Section 3 Content          
+          Your Section 4 Content
         </div>
       </section>
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,7 @@ export default {
     extend: {
       animation: {
         aurora: "aurora 60s linear infinite",
+        spotlight: "spotlight 2s ease .75s 1 forwards",
       },
       keyframes: {
         aurora: {
@@ -23,6 +24,16 @@ export default {
           },
           to: {
             backgroundPosition: "350% 50%, 350% 50%",
+          },
+        },
+        spotlight: {
+          "0%": {
+            opacity: "0",
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translate(-50%,-40%) scale(1)",
           },
         },
       },
