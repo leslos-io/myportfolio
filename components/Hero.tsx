c26b46087c3f6994059896fec3b177300ecd62bf
Leslos <lesliesamafful@gmail.com>
2025-01-20 11:19:53 +0000

    refactor Hero component to dynamically adjust background pattern size based on viewport width

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -152,9 +152,67 @@ const HeroRightMobile = () => {
   };
 
 const Hero = () => {
+
+  const [bgPatternSize, setBgPatternSize] = useState(200);
+
+  useEffect(() => {
+    const updateVisiblePeople = () => {
+      if (window.innerWidth <= 640) {
+        setBgPatternSize(150);
+      } else {
+        setBgPatternSize(200);
+      }
+    };
+
+    updateVisiblePeople();
+    window.addEventListener("resize", updateVisiblePeople);
+
+    return () => window.removeEventListener("resize", updateVisiblePeople);
+  }, []);
+
+
+
   return (
-    <div className="bg-gradient-to-r from-stone-950 via-zinc-900 to-zinc-950">
-      <div className="container mx-auto">
+    <div className="relative bg-gradient-to-tr from-stone-950/20 via-stone-700/20 to-stone-950/20">
+
+<svg
+        aria-hidden="true"
+        className="absolute inset-0 -z-10 size-full stroke-white/10 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
+      >
+        <defs>
+          <pattern
+            x="50%"
+            y={-1}
+            id="983e3e4c-de6d-4c3f-8d64-b9761d1534cc"
+            width={bgPatternSize}
+            height={bgPatternSize}
+            patternUnits="userSpaceOnUse"
+          >
+            <path d="M.5 200V.5H200" fill="none" />
+          </pattern>
+        </defs>
+        <svg x="50%" y={-1} className="overflow-visible fill-gray-800/20">
+          <path
+            d="M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z"
+            strokeWidth={0}
+          />
+        </svg>
+        <rect fill="url(#983e3e4c-de6d-4c3f-8d64-b9761d1534cc)" width="100%" height="100%" strokeWidth={0} />
+      </svg>
+      
+      <div
+        aria-hidden="true"
+        className="absolute top-10 left-[calc(50%-4rem)] -z-10 transform-gpu blur-3xl sm:left-[calc(50%-18rem)] lg:top-[calc(50%-30rem)] lg:left-48 xl:left-[calc(50%-24rem)]"
+      >
+        <div
+          style={{
+            clipPath:
+              'polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)',
+          }}
+          className="aspect-1108/632 w-[69.25rem] bg-linear-to-r from-[#80caff] to-[#4f46e5] opacity-20"
+        />
+      </div>
+      <div className="relative container mx-auto">
         {/* Mobile Layout */}
         <div className="lg:hidden">
           <section className="h-screen snap-center flex items-center justify-center">
@@ -167,37 +225,6 @@ const Hero = () => {
 
         {/* Desktop Layout */}
         <div className="hidden lg:flex lg:h-screen snap-center">
-
-        <svg
-          aria-hidden="true"
-          className="absolute inset-0 -z-0 size-full stroke-gray-500/10 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]" >
-          <defs>
-            <pattern
-              x="50%"
-              y={-1}
-              id="83fd4e5a-9d52-42fc-97b6-718e5d7ee527"
-              width={200}
-              height={200}
-              patternUnits="userSpaceOnUse"
-            >
-              <path d="M100 200V.5M.5 .5H200" fill="none" />
-            </pattern>
-          </defs>
-          <svg x="50%" y={-1} className="overflow-visible fill-gray-50">
-            <path
-              d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z"
-              strokeWidth={0}
-            />
-          </svg>
-          <rect
-            fill="url(#83fd4e5a-9d52-42fc-97b6-718e5d7ee527)"
-            width="100%"
-            height="100%"
-            strokeWidth={0}
-          />
-        </svg>
-
-
 
           <div className="w-1/2 flex items-center justify-end">
             <HeroLeft />
